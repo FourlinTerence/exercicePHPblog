ba859f2098670f95ece645dc9307cd784cb2dfa8
FourlinTerence <fourlinterence@gmail.com>
2023-06-15 15:13:49 +0400

    sauvegarde pour etre sur

---
+++ INCcarte.php
@@ -0,0 +1,69 @@
+<main>
+        <h1>TITRE CARTE</h1>
+        <div class="container">
+            <div class="carte">
+                <img src="image/carte redimensionnee.jpg" alt="" id="carte" />
+                <p>Description de la carte</p>
+            </div>
+            <div class="cartedescription">
+                <img src="image/341px-Tuit-tuit_(coracina_newtoni),_mâle_adulte.jpg" alt="" />
+                <p>
+                    Cupcake ipsum dolor sit amet cake croissant fruitcake muffin. Sweet
+                    bonbon candy powder cheesecake muffin sesame snaps jujubes. Muffin
+                    macaroon cheesecake cookie souffleacute; cookie cheesecake lollipop
+                    candy canes. Halvah cotton candy sesame snaps chocolate bar. Sugar
+                    plum dessert drageacute;e macaroon chupa chups. Cake cheesecake
+                    wafer cotton candy.
+                </p>
+            </div>
+        </div>
+
+        <div class="commentaire">
+            <h3>Commentaire</h3>
+            <div>
+                <p class="bold">Pseudo</p>
+                <p>
+                    Cupcake ipsum dolor sit fruitcake muffin. Sweet bonbon candy powder
+                    cheesecake muffin sesame snaps jujubes. Muffin macaroon cheesec
+                    cookie souffl peacute; cookie cheesecake lollipop candy canes.
+                    Halvah cotton candy sesame snaps chocolate bar. Suga
+                </p>
+            </div>
+            <div>
+                <p class="bold">Pseudo</p>
+                <p>
+                    Cupcake ipsum dolor sit fruitcake muffin. Sweet bonbon candy powder
+                    cheesecake muffin sesame snaps jujubes. Muffin macaroon cheesec
+                    cookie souffl peacute; cookie cheesecake lollipop candy canes.
+                    Halvah cotton candy sesame snaps chocolate bar. Suga
+                </p>
+            </div>
+            <div>
+                <p class="bold">Pseudo</p>
+                <p>
+                    Cupcake ipsum dolor sit fruitcake muffin. Sweet bonbon candy powder
+                    cheesecake muffin sesame snaps jujubes. Muffin macaroon cheesec
+                    cookie souffl peacute; cookie cheesecake lollipop candy canes.
+                    Halvah cotton candy sesame snaps chocolate bar. Suga
+                </p>
+            </div>
+            <div>
+                <p class="bold">Pseudo</p>
+                <p>
+                    Cupcake ipsum dolor sit fruitcake muffin. Sweet bonbon candy powder
+                    cheesecake muffin sesame snaps jujubes. Muffin macaroon cheesec
+                    cookie souffl peacute; cookie cheesecake lollipop candy canes.
+                    Halvah cotton candy sesame snaps chocolate bar. Suga
+                </p>
+            </div>
+            <div>
+                <p class="bold">Pseudo</p>
+                <p>
+                    Cupcake ipsum dolor sit fruitcake muffin. Sweet bonbon candy powder
+                    cheesecake muffin sesame snaps jujubes. Muffin macaroon cheesec
+                    cookie souffl peacute; cookie cheesecake lollipop candy canes.
+                    Halvah cotton candy sesame snaps chocolate bar. Suga
+                </p>
+            </div>
+        </div>
+    </main>
