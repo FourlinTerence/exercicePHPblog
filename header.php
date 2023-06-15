2d27c35216cfd6ea3161107390db20fcee652528
FourlinTerence <fourlinterence@gmail.com>
2023-06-15 14:25:07 +0400

    first commit

---
+++ header.php
@@ -0,0 +1,17 @@
+<header>
+    <a href="acceuil.html"><img class="logonav" src="image/coqlakour-removebg-preview.png" alt="" /></a>
+    <nav>
+        <a href="contact.html">Contact</a>
+        <a href="information.html">information.html</a>
+        <a href="connexion.html">Connection</a>
+    </nav>
+
+    <div class="burger dropdown">
+        <button class="dropbtn"><img src="image/lines_menu_burger_icon_123889_encorepluspetit.png" alt=""></button>
+        <div class="dropdown-content">
+            <a href="contact.html">Contact</a>
+            <a href="information.html">Information</a>
+            <a href="connexion.html">Connection</a>
+        </div>
+    </div>
+</header>
