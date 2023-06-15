7e11f999ac05dff22b043c46282dd430eea6b93c
FourlinTerence <fourlinterence@gmail.com>
2023-06-15 17:08:54 +0400

    Creation page article carte et connexion

---
+++ article.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php'; ?>
+
+<body>
+  <?php include 'header.php'; ?>
+  <?php include 'INCarticle.php'; ?>
+  <?php include 'footer.php'; ?>
+</body>
+
+</html>
---
+++ connexion.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php'; ?>
+
+<body>
+  <?php include 'header.php'; ?>
+  <?php include 'connexion.php'; ?>
+  <?php include 'footer.php'; ?>
+</body>
+
+</html>
