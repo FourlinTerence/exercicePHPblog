ff828a0c065e52c3113f665dc337560ff5aeb295
FourlinTerence <fourlinterence@gmail.com>
2023-06-15 17:15:09 +0400

    creation page contact formulaire article formulaire carte information et lien dans le header

--- header.php
+++ header.php
@@ -1,17 +1,17 @@
 <header>
     <a href="index.php"><img class="logonav" src="image/coqlakour-removebg-preview.png" alt="Logo coqlakour" /></a>
     <nav>
-        <a href="">Contact</a>
-        <a href="">information</a>
-        <a href="">Connection</a>
+        <a href="contact.php">Contact</a>
+        <a href="information.php">information</a>
+        <a href="connexion.php">Connection</a>
     </nav>
 
     <div class="burger dropdown">
         <button class="dropbtn"><img src="image/lines_menu_burger_icon_123889_encorepluspetit.png" alt=""></button>
         <div class="dropdown-content">
-            <a href="">Contact</a>
-            <a href="">Information</a>
-            <a href="">Connection</a>
+            <a href="contact.php">Contact</a>
+            <a href="information.php">Information</a>
+            <a href="connexion.php">Connection</a>
         </div>
     </div>
 </header>
