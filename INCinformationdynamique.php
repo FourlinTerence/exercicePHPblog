a2e8f3f31d4001ca2b04c989b4e76436cf628dc0
FourlinTerence <fourlinterence@gmail.com>
2023-06-20 05:32:17 +0400

    Debut de creation des article dynamique

---
+++ INCinformationdynamique.php
@@ -0,0 +1,87 @@
+<main>
+
+    <h1>INFORMATION</h1>
+
+    <section class="pageinfo">
+        <div class="infobox">
+            <h3>Nos articles</h3>
+            <div class="infoarticle">
+                <a href="index.php?page=article"><img src="image/lunette vert retrecie.png" alt=""></a><br>
+                <div class="infos">
+                    <p class="bold">Titre de l'article</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem quo, numquam velit expedita... <span class="italic"><a href="index.php?page=article">Lire l'article</a></span></p>
+                    <p>catégorie de l'article</p>
+                    <p class="italic">09/06/2023</p>
+                </div>
+            </div>
+            <div class="infoarticle">
+                <a href="index.php?page=article"><img src="image/lunette vert retrecie.png" alt=""></a><br>
+                <div class="infos">
+                    <p class="bold">Titre de l'article</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem quo, numquam velit expedita... <span class="italic"><a href="index.php?page=article">Lire l'article</a></span></p>
+                    <p>catégorie de l'article</p>
+                    <p class="italic">09/06/2023</p>
+                </div>
+            </div>
+            <div class="infoarticle">
+                <a href="index.php?page=article"><img src="image/lunette vert retrecie.png" alt=""></a><br>
+                <div class="infos">
+                    <p class="bold">Titre de l'article</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem quo, numquam velit expedita... <span class="italic"><a href="index.php?page=article">Lire l'article</a></span></p>
+                    <p>catégorie de l'article</p>
+                    <p class="italic">09/06/2023</p>
+                </div>
+            </div>
+            <div class="infoarticle">
+                <a href="index.php?page=article"><img src="image/lunette vert retrecie.png" alt=""></a><br>
+                <div class="infos">
+                    <p class="bold">Titre de l'article</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem quo, numquam velit expedita... <span class="italic"><a href="index.php?page=article">Lire l'article</a></span></p>
+                    <p>catégorie de l'article</p>
+                    <p class="italic">09/06/2023</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="infobox">
+            <h3>Nos cartes</h3>
+            <div class="infoarticle">
+                <a href="index.php?page=carte"><img src="image/lunette vert retrecie.png" alt=""></a><br>
+                <div class="infos">
+                    <p class="bold">Titre de la carte</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem quo, numquam velit expedita... <span class="italic"><a href="index.php?page=carte">Lire l'article</a></span></p>
+                    <p>catégorie de la carte</p>
+                    <p class="italic">09/06/2023</p>
+                </div>
+            </div>
+            <div class="infoarticle">
+                <a href="index.php?page=carte"><img src="image/lunette vert retrecie.png" alt=""></a><br>
+                <div class="infos">
+                    <p class="bold">Titre de la carte</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem quo, numquam velit expedita... <span class="italic"><a href="index.php?page=carte">Lire l'article</a></span></p>
+                    <p>catégorie de la carte</p>
+                    <p class="italic">09/06/2023</p>
+                </div>
+            </div>
+            <div class="infoarticle">
+                <a href="index.php?page=carte"><img src="image/lunette vert retrecie.png" alt=""></a><br>
+                <div class="infos">
+                    <p class="bold">Titre de la carte</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem quo, numquam velit expedita... <span class="italic"><a class="lienarticle" href="index.php?page=carte">Lire l'article</a></span></p>
+                    <p>catégorie de la carte</p>
+                    <p class="italic">09/06/2023</p>
+                </div>
+            </div>
+            <div class="infoarticle">
+                <a href="index.php?page=carte"><img src="image/lunette vert retrecie.png" alt=""></a><br>
+                <div class="infos">
+                    <p class="bold">Titre de la carte</p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem quo, numquam velit expedita... <span class="italic"><a href="index.php?page=carte">Lire l'article</a></span></p>
+                    <p>catégorie de la carte</p>
+                    <p class="italic">09/06/2023</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <img class="infologocoqlakour" src="image/coqlakour-removebg-preview.png" alt="logo coqlakour">
+</main>
